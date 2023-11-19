6c4162eb3f81ae7f71712881f7a417a5a34021c9
ni00 <mail@nixiaobai.com>
2023-11-19 17:11:21 +0800

    1.5

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package monkey
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Mon2key programming language!\n", currentUser.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+}
